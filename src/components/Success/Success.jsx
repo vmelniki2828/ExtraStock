b199f0f89d2f5fdff082256f7c057dd2c391b5c2
vmelniki2828 <vmelniki2828@gmail.com>
2025-01-26 20:47:16 +0200

    edits

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,9 +1,15 @@
 import {
   DarkContainer,
+  DownArrow,
+  DownloadButton,
+  InputContainer,
   SuccessBlackText,
   SuccessContainer,
+  SuccessInput,
   SuccessMainTitle,
   SuccessPurple,
+  SuccessWhiteSubText,
+  SuccessWhiteSubTextStrong,
   SuccessWhiteText,
 } from './Success.styled';
 
@@ -17,6 +23,29 @@ const Success = () => {
       <SuccessBlackText>Бесплатный чек-лист по</SuccessBlackText>
       <DarkContainer>
         <SuccessWhiteText>открытию магазина секонд-хенд</SuccessWhiteText>
+        <SuccessWhiteSubText>
+          100% рецепта успеха в бизнесе не существует,
+          <SuccessWhiteSubTextStrong>
+            {' '}
+            но сократить до минимума риски, понять как правильно делать то или
+            иное жизнено важно.
+          </SuccessWhiteSubTextStrong>{' '}
+          Мы бесплатно отправим Вам чек-лист открытия{' '}
+          <SuccessWhiteSubTextStrong>
+            Успешного магазина секонд-хенд
+          </SuccessWhiteSubTextStrong>
+          . В нем перечислен ряд вопросов которые мы обсуждаем на наших
+          вебинирах.
+        </SuccessWhiteSubText>
+        <InputContainer>
+          <SuccessInput placeholder="ИМЯ..." />
+          <SuccessInput placeholder="ЕЛЕКТРОННАЯ ПОЧТА..." />
+          <SuccessInput placeholder="НОМЕР ТЕЛЕФОНА..." />
+        </InputContainer>
+        <DownloadButton>
+          Скачать чек-лист
+          <DownArrow />
+        </DownloadButton>
       </DarkContainer>
     </SuccessContainer>
   );
--- src/components/Success/Success.styled.jsx
+++ src/components/Success/Success.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { IoIosArrowRoundDown } from 'react-icons/io';
 
 export const SuccessContainer = styled.div`
   display: flex;
@@ -62,4 +63,81 @@ export const DarkContainer = styled.div`
   flex-direction: column;
   align-items: center;
   background: black;
+`;
+
+export const SuccessWhiteSubText = styled.p`
+  margin: 0;
+  width: 1260px;
+  margin-top: 60px;
+  font-family: 'Geologica', sans-serif;
+  font-style: normal;
+  font-weight: 200;
+  font-size: 43px;
+  line-height: 58px;
+  text-align: center;
+  text-transform: uppercase;
+
+  color: #ffffff;
+`;
+
+export const SuccessWhiteSubTextStrong = styled.span`
+  font-weight: 500;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  margin-bottom: 80px;
+  gap: 30px;
+`;
+
+export const SuccessInput = styled.input`
+  width: 326px;
+  height: 88px;
+  background: rgba(255, 255, 255, 0.08);
+  border: 1.5035px solid #ffffff;
+  border-radius: 75.1748px;
+  padding: 0 36px;
+
+  font-family: 'Geologica', sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 27px;
+  line-height: 27px;
+  text-transform: lowercase;
+
+  color: #9c9c9c;
+`;
+
+export const DownloadButton = styled.div`
+  margin: 0 auto;
+  color: #fff;
+  width: 395px;
+  height: 87px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  border: 1px solid #ffc400;
+  border-radius: 43.5px;
+
+  font-family: 'Geologica', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 33px;
+  line-height: 18px;
+  padding-left: 18px;
+
+  color: #ffffff;
+  margin-bottom: 77px;
+`;
+
+export const DownArrow = styled(IoIosArrowRoundDown)`
+  background: transparent;
+  border-radius: 43.5px;
+  border: 1px solid #ffc300;
+  width: 73px;
+  height: 73px;
+  margin-right: -4px;
 `;
